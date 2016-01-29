9a34f82d29f5a1cb098bf84935787db95689c29c
David Stendardi <dstendardi@viadeoteam.com>
2016-01-28 22:32:08 -0800

    add java service for example

---
+++ service-c/src/test/java/servicec/MainTest.java
@@ -0,0 +1,12 @@
+package servicec;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+  
+   @Test
+   public void testTheTruth() {
+      assertEquals(true, true);
+   }
+}
